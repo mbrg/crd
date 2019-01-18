eee87394d5565479c9a4350e794bef5850fbb5e8
mibarg <11074433+mibarg@users.noreply.github.com>
2019-01-18 13:21:53 +0200

    Azure KeyVault Storage

---
+++ nomorepass/azure_strg.py
@@ -0,0 +1,75 @@
+from azure.keyvault.models import KeyVaultErrorException, SecretItemPaged
+from azure.keyvault import KeyVaultClient, KeyVaultAuthentication, KeyVaultId
+import adal
+
+from storage import Storage
+
+
+class AzureKeyVaultStorage(Storage):
+    """
+    Azure KeyVault persistent Storage
+    Based on: https://github.com/Azure/azure-sdk-for-python/blob/master/
+              azure-keyvault/azure/keyvault/v2016_10_01/key_vault_client.py
+    """
+
+    @staticmethod
+    def init_connection(tenant_id, session_mem):
+
+        auth_context = adal.AuthenticationContext('https://login.microsoftonline.com/%s' % tenant_id)
+        xplat_client_id = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'  # Azure CLI
+
+        def auth_handler(server, resource, scope):
+            if (server, resource, scope) not in session_mem:
+                user_code_info = auth_context.acquire_user_code(resource,
+                                                                xplat_client_id)
+
+                print(user_code_info['message'])
+                session_mem[(server, resource, scope)] = auth_context.acquire_token_with_device_code(
+                    resource=resource, client_id=xplat_client_id, user_code_info=user_code_info)
+
+            token = session_mem[(server, resource, scope)]
+            return token['tokenType'], token['accessToken']
+
+        conn = KeyVaultClient(KeyVaultAuthentication(auth_handler))
+
+        return conn
+
+    def __init__(self, vault : str, tenant_id : str, *args, **kwargs):
+        self.vault = vault
+
+        self.session_mem = {}
+        self.conn = self.init_connection(tenant_id, self.session_mem)
+
+        self.update(dict(*args, **kwargs))
+
+    @property
+    def vault_uri(self):
+        return "https://%s.vault.azure.net/" % self.vault
+
+    def __getitem__(self, key):
+        try:
+            secret_bundle = self.conn.get_secret(self.vault_uri, key, KeyVaultId.version_none)
+            return secret_bundle.value
+        except KeyVaultErrorException as e:
+            raise KeyError('KeyVaultErrorException: ' + e.message)
+
+    def __setitem__(self, key, value):
+        self.conn.set_secret(self.vault_uri, key, value)
+
+    def __delitem__(self, key):
+        try:
+            self.conn.delete_secret(self.vault_uri, key)
+        except KeyVaultErrorException as e:
+            raise KeyError('KeyVaultErrorException: ' + e.message)
+
+    def __iter__(self):
+        secret_item_paged = self.conn.get_secrets(self.vault_uri)
+
+        for sec in secret_item_paged:
+            yield sec.id.replace(self.vault_uri + 'secrets/', '')
+
+    def __len__(self):
+        return len(list(self.__iter__()))
+
+    def __str__(self):
+        return '%s(%s)' % (self.__class__.__name__, self.vault)
